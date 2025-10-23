a13f5fdd73fa3fe02f8b64b7c31456ab4d082a13
Eshan <eshan.savla@ipa.fraunhofer.de>
2025-10-23 17:54:22 +0200

    test script for .ros model

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,55 +1,70 @@
-'use strict';
+// 'use strict';
+
+// import * as net from 'net';
+
+// import {Trace} from 'vscode-jsonrpc';
+// import { window, workspace, commands, ExtensionContext, Uri } from 'vscode';
+// import { LanguageClient, LanguageClientOptions, StreamInfo, Position as LSPosition, Location as LSLocation } from 'vscode-languageclient/node';
+
+// let lc: LanguageClient;
+
+// export function activate(context: ExtensionContext) {
+//     // The server is a started as a separate app and listens on port 5007
+//     let connectionInfo = {
+//         port: 5007
+//     };
+//     let serverOptions = () => {
+//         // Connect to language server via socket
+//         let socket = net.connect(connectionInfo);
+//         let result: StreamInfo = {
+//             writer: socket,
+//             reader: socket
+//         };
+//         return Promise.resolve(result);
+//     };
+    
+//     let clientOptions: LanguageClientOptions = {
+//         documentSelector: ['mydsl'],
+//         synchronize: {
+//             fileEvents: workspace.createFileSystemWatcher('**/*.*')
+//         }
+//     };
+    
+//     // Create the language client and start the client.
+//     lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
+
+//     var disposable2 =commands.registerCommand("mydsl.a.proxy", async () => {
+//         let activeEditor = window.activeTextEditor;
+//         if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'mydsl') {
+//             return;
+//         }
+
+//         if (activeEditor.document.uri instanceof Uri) {
+//             commands.executeCommand("mydsl.a", activeEditor.document.uri.toString());
+//         }
+//     })
+
+//     context.subscriptions.push(disposable2);
+
+//     // enable tracing (.Off, .Messages, Verbose)
+//     lc.setTrace(Trace.Verbose);
+//     lc.start();
+// }
+// export function deactivate() {
+//     return lc.stop();
+// }
 
 import * as net from 'net';
+import { LanguageClient, LanguageClientOptions, StreamInfo } from 'vscode-languageclient/node';
 
-import {Trace} from 'vscode-jsonrpc';
-import { window, workspace, commands, ExtensionContext, Uri } from 'vscode';
-import { LanguageClient, LanguageClientOptions, StreamInfo, Position as LSPosition, Location as LSLocation } from 'vscode-languageclient/node';
-
-let lc: LanguageClient;
-
-export function activate(context: ExtensionContext) {
-    // The server is a started as a separate app and listens on port 5007
-    let connectionInfo = {
-        port: 5007
-    };
-    let serverOptions = () => {
-        // Connect to language server via socket
-        let socket = net.connect(connectionInfo);
-        let result: StreamInfo = {
-            writer: socket,
-            reader: socket
-        };
-        return Promise.resolve(result);
-    };
-    
-    let clientOptions: LanguageClientOptions = {
-        documentSelector: ['mydsl'],
-        synchronize: {
-            fileEvents: workspace.createFileSystemWatcher('**/*.*')
-        }
-    };
-    
-    // Create the language client and start the client.
-    lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
-
-    var disposable2 =commands.registerCommand("mydsl.a.proxy", async () => {
-        let activeEditor = window.activeTextEditor;
-        if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'mydsl') {
-            return;
-        }
-
-        if (activeEditor.document.uri instanceof Uri) {
-            commands.executeCommand("mydsl.a", activeEditor.document.uri.toString());
-        }
-    })
-
-    context.subscriptions.push(disposable2);
-
-    // enable tracing (.Off, .Messages, Verbose)
-    lc.setTrace(Trace.Verbose);
-    lc.start();
+function createServer(): Promise<StreamInfo> {
+  const socket = net.connect(5008, '127.0.0.1');
+  return Promise.resolve({ reader: socket, writer: socket });
 }
-export function deactivate() {
-    return lc.stop();
-}
+
+const clientOptions: LanguageClientOptions = {
+  documentSelector: [{ scheme: 'file', language: 'ros' }]
+};
+
+const client = new LanguageClient('rosLS', 'ROS Language Server', createServer, clientOptions);
+client.start();
